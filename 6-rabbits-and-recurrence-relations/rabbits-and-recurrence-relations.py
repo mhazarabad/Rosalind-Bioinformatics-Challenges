86a2b7620e66d803e4e14c2d2b9a1f2882c26bd3
M Hadi Azarabad <mhazarabad@gmail.com>
2023-11-04 20:54:15 +0100

    weighted fibonachi added with recurssive function, and limit 1999 nth

---
+++ 6-rabbits-and-recurrence-relations/rabbits-and-recurrence-relations.py
@@ -0,0 +1,22 @@
+calculated_fibonachies={1:1,2:1}
+def Weighted_Fibonachi_Calculator(weight:int,idx:int)->int:
+    """
+    
+    ## params
+        idx: an integer represent
+        weight: an integer corresponds for the number of pair rabbits that will produced from re-productive rabbits (at least 1 month old)
+    
+    """
+    re_production_age=calculated_fibonachies.get(idx-2)
+    if not re_production_age:
+        re_production_age=calculated_fibonachies[idx-2]=Weighted_Fibonachi_Calculator(weight=weight,idx=idx-2)
+    
+    none_re_production_age=calculated_fibonachies.get(idx-1)
+    if not none_re_production_age:
+        none_re_production_age=calculated_fibonachies[idx-1]=Weighted_Fibonachi_Calculator(weight=weight,idx=idx-1)
+    
+    return weight*re_production_age+none_re_production_age
+
+print(Weighted_Fibonachi_Calculator(
+    idx=600,weight=5
+))
